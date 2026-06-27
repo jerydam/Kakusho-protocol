5814e8165eeca329b6fc092569a4e6a660c1fede
Jerydam <jerydan148@gmail.com>
2026-06-27 03:28:21 +0100

    add data dir placeholder for Docker build

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
     return [
       {
         source: '/api/kakusho/:path*',
-        destination: 'http://127.0.0.1:8000/:path*',
+        destination: 'https://worrying-drucy-faucetdrops-aab2b1e1.koyeb.app/:path*',
       },
     ];
   },
